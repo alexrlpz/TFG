3acc751f0f080650e1e83003e3a6d52d803ac232
Alejandro Rueda López <arl19@correo.ugr.es>
2024-04-22 19:25:45 +0200

    Added firing rates correlation to plots

--- LIF_model/plots.py
+++ LIF_model/plots.py
@@ -4,14 +4,35 @@
 from time import time
 import nest
 import os
+import math
 import pickle
 import numpy as np
 import scipy.signal as ss
 import matplotlib.pyplot as plt
 import LIF_network
-'''
-To do.
-'''
+import random
+import scipy
+from multiprocessing import Pool
+
+def pairwise_correlation_worker(args):
+    x, i, j = args
+    c, p_value = scipy.stats.pearsonr(x[i], x[j])
+    return c if not math.isnan(c) else None
+
+def pairwise_correlation(x, bin=200, hist_range=[-1.0, 1.0], num_processes=4):
+    cc = []
+    args_list = []
+    for i in range(len(x)):
+        for j in range(i + 1, len(x)):
+            args_list.append((x, i, j))
+
+    with Pool(num_processes) as pool:
+        results = pool.map(pairwise_correlation_worker, args_list)
+
+    cc = [c for c in results if c is not None]
+    hist, bin_edges = np.histogram(cc, bins=bin, range=hist_range)
+
+    return np.mean(cc), hist, bin_edges
 
 def zscore(data, ch, time):
     '''
@@ -221,4 +242,41 @@ plt.text(1.05, 0.85, 'Parámetros: \n J_EE: ' + str(LIF_net.LIF_params['J_YX'][0
                            '\n J_EI: ' + str(LIF_net.LIF_params['J_YX'][1][0]) + 
                            '\n J_II:  ' + str(LIF_net.LIF_params['J_YX'][1][1])                   
                             ,transform=ax1.transAxes, verticalalignment='bottom', fontsize=8) 
+
+# Firing rates correlation between pairs of neurons
+
+gids_E = pickle.load(open(path+'/gids_'+'E','rb')) 
+times_E = pickle.load(open(path+'/times_'+'E','rb'))
+
+gids_E = gids_E[times_E >= LIF_net.TRANSIENT]
+times_E = times_E[times_E >= LIF_net.TRANSIENT]
+NUM_NEURONS = 500
+BIN_TIME = 80
+NUM_BINS = int((LIF_net.tstop-LIF_net.TRANSIENT)/BIN_TIME)
+
+random_neurons = [random.randint(1, 8192) for _ in range(NUM_NEURONS)]
+
+total_firing_rates = np.zeros((NUM_NEURONS, NUM_BINS))
+bin_edges = np.arange(LIF_net.TRANSIENT, LIF_net.tstop+BIN_TIME, BIN_TIME)
+
+for i, neuron_id in enumerate(random_neurons):
+    neuron_spikes_times = times_E[gids_E == neuron_id]                              # Times of the spikes produced by this neuron
+    total_firing_rates[i], _ = np.histogram(neuron_spikes_times, bins=bin_edges)
+
+total_firing_rates = total_firing_rates / BIN_TIME
+    
+if __name__ == "__main__":
+    mean_corr, hist, bin_edges = pairwise_correlation(total_firing_rates, bin=200, hist_range=[-1.0, 1.0], num_processes=12)
+
+plt.figure(figsize=(8, 6))
+plt.bar(bin_edges[:-1], hist, width=np.diff(bin_edges), align='edge', edgecolor='black')
+plt.xlabel('Correlation')
+plt.ylabel('Frequency')
+plt.title('Firing rates correlation histogram')
+plt.axvline(mean_corr, color='r', linestyle='dashed', linewidth=1, label=f'Media: {mean_corr:.2f}')
+plt.legend()
+plt.grid(True)
+
 plt.show()
+
+
